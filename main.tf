b27a4221f5f04e65d890b73a3d3c7e69a9855d17
Faisal Saeed <faisal.saeed@scylladb.com>
2024-05-28 05:44:29 +0800

    Added support for remote execution

--- main.tf
+++ main.tf
@@ -9,20 +9,33 @@ provider "random" {
 
 resource "google_compute_instance" "scylla-loader" {
   count        = 3  # Number of instances to create
-  name         = "scylla-loader-${format("%02d", count.index + 1)}"
+  name         = "faisal-scylla-loader-${format("%02d", count.index + 1)}"
   machine_type = "n2-highmem-2"
   zone         = var.zone
   min_cpu_platform = "Intel Ice Lake"
   tags = ["keep", "alive", "ssh"]
 
   metadata = {
-    ssh-keys = "root:${file(var.ssh_public_key_path)}"
+    ssh-keys = "ubuntu:${file(var.ssh_public_key_path)}"
+  }
+  
+  provisioner "remote-exec" {
+    inline = [
+      "sudo apt update",
+    ]
+      connection {
+      type        = "ssh"
+      user        = "ubuntu"
+      private_key = file(var.ssh_private_public_key_path)
+      host        = self.network_interface.0.access_config.0.nat_ip
+    }
   }
 
   boot_disk {
-    initialize_params {
-      image = "scylla-images/scylladb-enterprise-2024-1-2"
-    }
+      initialize_params {
+        //image = "ubuntu-os-cloud/ubuntu-2204-lts"
+        image = "ubuntu-2204-lts"
+      }
   }
 
   scratch_disk {
@@ -52,4 +65,14 @@ resource "google_compute_firewall" "allow_ssh" {
 
 resource "random_id" "firewall_suffix" {
   byte_length = 2
+}
+
+output "internal_ips" {
+  value = google_compute_instance.scylla-loader[*].network_interface.0.network_ip
+  description = "Iternal IP addresses of the instances"
+}
+
+output "public_ips" {
+  value = google_compute_instance.scylla-loader[*].network_interface.0.access_config.0.nat_ip
+  description = "Public IP addresses of the instances"
 }
--- variables.tf
+++ variables.tf
@@ -21,3 +21,10 @@ variable "ssh_public_key_path" {
   default     = "~/.ssh/id_ed25519.pub"
 }
 
+variable "ssh_private_public_key_path" {
+  description = "Path to the SSH private key file"
+  type        = string
+  default     = "~/.ssh/id_ed25519"
+}
+
+

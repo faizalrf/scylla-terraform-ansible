e92468e3266b16b4cd37a92b24084f7918dff318
Faisal Saeed <faisal.saeed@scylladb.com>
2024-06-08 23:15:01 +0800

    Working Ansible & Terraform

---
+++ go.sh
@@ -0,0 +1,5 @@
+terraform init
+terraform destroy -auto-approve
+terraform plan
+terraform apply -auto-approve
+./inventory.sh
--- main.tf
+++ main.tf
@@ -17,10 +17,10 @@ provider "random" {
   // Nothing to do here
 }
 
-resource "google_compute_instance" "scylla-loader" {
+resource "google_compute_instance" "scylla-node" {
   count        = 3
-  name         = "faisal-scylla-loader-${format("%02d", count.index + 1)}"
-  machine_type = "n2-highmem-4"
+  name         = "faisal-scylla-node-${format("%02d", count.index + 1)}"
+  machine_type = "n2-highmem-2"
   zone         = var.zone
   min_cpu_platform = "Intel Ice Lake"
   tags = ["keep", "alive", "ssh"]
@@ -29,24 +29,28 @@ resource "google_compute_instance" "scylla-loader" {
     ssh-keys = "ubuntu:${file(var.ssh_public_key_path)}"
   }
   
-  provisioner "remote-exec" {
-    #Create the NVMe filesystem and mount /var/lib to it for ScyllaDB to use
-    inline = [
-      "sudo apt update -y",
-    ]
-      connection {
-      type        = "ssh"
-      user        = "ubuntu"
-      private_key = file(var.ssh_private_public_key_path)
-      host        = self.network_interface.0.access_config.0.nat_ip
-    }
-  }
+  #provisioner "remote-exec" {
+  #  #Create the NVMe filesystem and mount /var/lib to it for ScyllaDB to use
+  #  inline = [
+  #    "sudo apt update -y",
+  #  ]
+  #    connection {
+  #    type        = "ssh"
+  #    user        = "ubuntu"
+  #    private_key = file(var.ssh_private_public_key_path)
+  #    host        = self.network_interface.0.access_config.0.nat_ip
+  #  }
+  #}
 
   boot_disk {
     initialize_params {
       image = "ubuntu-2204-lts"
-      size = 256
+      #size = 256
     }
+  }
+
+  scratch_disk {
+    interface = "NVME"
   }
 
   network_interface {
@@ -70,16 +74,94 @@ resource "google_compute_firewall" "allow_ssh" {
   target_tags   = ["ssh"]
 }
 
+resource "google_compute_firewall" "allow_cql" {
+  name    = "allow-cql-${random_id.firewall_suffix.hex}"
+  network = "default"
+
+  allow {
+    protocol = "tcp"
+    ports    = ["9042"]
+  }
+
+  source_ranges = ["0.0.0.0/0"]
+  target_tags   = ["scylla"]
+}
+
+resource "google_compute_firewall" "allow_thrift" {
+  name    = "allow-thrift-${random_id.firewall_suffix.hex}"
+  network = "default"
+
+  allow {
+    protocol = "tcp"
+    ports    = ["9160"]
+  }
+
+  source_ranges = ["0.0.0.0/0"]
+  target_tags   = ["scylla"]
+}
+
+resource "google_compute_firewall" "allow_internode" {
+  name    = "allow-internode-${random_id.firewall_suffix.hex}"
+  network = "default"
+
+  allow {
+    protocol = "tcp"
+    ports    = ["7000", "7001"]
+  }
+
+  source_ranges = ["0.0.0.0/0"]
+  target_tags   = ["scylla"]
+}
+
+resource "google_compute_firewall" "allow_jmx" {
+  name    = "allow-jmx-${random_id.firewall_suffix.hex}"
+  network = "default"
+
+  allow {
+    protocol = "tcp"
+    ports    = ["7199"]
+  }
+
+  source_ranges = ["0.0.0.0/0"]
+  target_tags   = ["scylla"]
+}
+
+resource "google_compute_firewall" "allow_rest_api" {
+  name    = "allow-rest-api-${random_id.firewall_suffix.hex}"
+  network = "default"
+
+  allow {
+    protocol = "tcp"
+    ports    = ["10000"]
+  }
+
+  source_ranges = ["0.0.0.0/0"]
+  target_tags   = ["scylla"]
+}
+
+resource "google_compute_firewall" "allow_alternator" {
+  name    = "allow-alternator-${random_id.firewall_suffix.hex}"
+  network = "default"
+
+  allow {
+    protocol = "tcp"
+    ports    = ["8000"]
+  }
+
+  source_ranges = ["0.0.0.0/0"]
+  target_tags   = ["scylla"]
+}
+
 resource "random_id" "firewall_suffix" {
   byte_length = 2
 }
 
 output "internal_ips" {
-  value = google_compute_instance.scylla-loader[*].network_interface.0.network_ip
+  value = google_compute_instance.scylla-node[*].network_interface.0.network_ip
   description = "Iternal IP addresses of the instances"
 }
 
 output "public_ips" {
-  value = google_compute_instance.scylla-loader[*].network_interface.0.access_config.0.nat_ip
+  value = google_compute_instance.scylla-node[*].network_interface.0.access_config.0.nat_ip
   description = "Public IP addresses of the instances"
 }

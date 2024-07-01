28c2944072b9f69be8043a3a0f6439464773d718
Faisal Saeed <faisal.saeed@scylladb.com>
2024-07-01 20:44:20 +0800

    Check if `jq` is installed

--- go.sh
+++ go.sh
@@ -1,6 +1,14 @@
 # Very simple script to init, destroy, plan and apply the terraform script
 # The last line gen-inventory.sh will read the IP addresses deployed by terraform and generata the ansible inventory file
 # Faisal Saeed @ ScyllaDB
+# Check if jq is installed
+if command -v jq >/dev/null 2>&1; then
+  echo "jq is installed."
+else
+  echo "jq is not installed. Please install it before proceeding"
+  exit 1
+fi
+
 terraform init
 terraform destroy -auto-approve
 terraform plan

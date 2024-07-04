be99a4d4485539aa3df4cb2b842e872d4a2b745d
Faisal Saeed <faisal.saeed@scylladb.com>
2024-07-04 19:26:37 +0800

    Fixed incorrect comment

--- ansible-play.sh
+++ ansible-play.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# This script reads the terraform output variables and generates an inventor file for Ansible
+# The additional command options here are to include the extra library files maintained for Scylla Ansible Role
 # Faisal Saeed @ ScyllaDB
 async_extra=./scylla-ansible-roles/example-playbooks/async_extra
 ANSIBLE_ACTION_PLUGINS="${async_extra}"/action_plugins ansible-playbook scylla-deploy.yml -M "${async_extra}"/library

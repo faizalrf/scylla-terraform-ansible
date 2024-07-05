922832c04db3e65a5d308d91fcc4c52672589ca3
Faisal Saeed <faisal.saeed@scylladb.com>
2024-07-05 19:15:49 +0800

    Removed the private key variable as not needed

--- terraform.tfvars
+++ terraform.tfvars
@@ -4,4 +4,3 @@ node_count = 3
 hardware_type = "n2-highmem-2"
 name_prefix = "ansible-role-test"
 ssh_public_key_path = "~/.ssh/id_ed25519.pub"
-ssh_private_public_key_path = "~/.ssh/id_ed25519"
--- variables.tf
+++ variables.tf
@@ -35,11 +35,3 @@ variable "ssh_public_key_path" {
   type        = string
   default     = "~/.ssh/id_ed25519.pub"
 }
-
-variable "ssh_private_public_key_path" {
-  description = "Path to the SSH private key file"
-  type        = string
-  default     = "~/.ssh/id_ed25519"
-}
-
-
